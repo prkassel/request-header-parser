8933a7591fd30584ef843f35eb68f7e49e5849dd
Philip <prkassel@gmail.com>
2016-05-05 14:54:33 -0500

    ip, language, and software

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var port = process.env.PORT || 8080;
 app.enable('trust proxy');
 
 app.get('/', function(req, res) {
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.json({ip: ip});
+  var obj = {
+    ip: req.ip,
+    language: req.get('accept-language').split(',')[0],
+    operatingSystem: req.get('user-agent').split(')')[0].split('(')[1]
+  }
+  res.json(obj);
 });
 
 app.listen(port, function() {
