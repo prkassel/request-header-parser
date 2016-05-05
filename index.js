36554e8ea31b5504117f2a5707692c0d25ad22d1
Philip <prkassel@gmail.com>
2016-05-05 13:36:04 -0500

    heroku deployment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 app.enable('trust proxy');
 
 app.get('/', function(req, res) {
@@ -7,6 +8,6 @@ app.get('/', function(req, res) {
   res.json({ip: ip});
 });
 
-app.listen(3000, function() {
-  console.log('Now listening on port 3000');
+app.listen(port, function() {
+  console.log('Now listening on port ' + port);
 })
